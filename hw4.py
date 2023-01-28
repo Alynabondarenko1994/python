22bbc1d402f0e83c62dcaba5db0cd2731c37cbd1
1 <bondarenko-1994alenka@mail.ru>
2023-01-28 23:18:43 +0300

    hw4

--- hw4.py
+++ hw4.py
@@ -6,7 +6,7 @@
 n=int(input('Введите число n:'))
 m=int(input('Введите число m:'))
 k=int(input('Введите число k:'))
-if (k<n*m) and ((k%n==0) or (k%m==0)):
+if (k<n*m) and ((k%n == 0) or (k%m == 0)):
     print ('Yes')
 else:
     print ('No')
